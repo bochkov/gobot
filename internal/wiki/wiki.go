6ce47feaf56f0cf220f04307841d33800ef5ae97
Sergey Bochkov <bochkovsa@gmail.com>
2024-03-26 18:37:50 +0500

    fix img

--- internal/wiki/wiki.go
+++ internal/wiki/wiki.go
@@ -20,5 +20,5 @@ type Service interface {
 
 func (t *ThisDay) AsHtml() string {
 	// https://stackoverflow.com/questions/38685619/how-to-send-an-embedded-image-along-with-text-in-a-message-via-telegram-bot-api/43705283#43705283
-	return fmt.Sprintf("<b>%s</b>\n<a href='%s'>&#8205;</a>%s", t.Date, t.ImgSrc, t.Text)
+	return fmt.Sprintf("<a href='%s'>&#8205;</a><b>%s</b>\n%s", t.ImgSrc, t.Date, t.Text)
 }
